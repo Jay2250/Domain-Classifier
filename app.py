7ba6349f5f880d09ba6f254a6e8af2eceec12ba5
Jay2250 <jayeshchauriwar@gmail.com>
2025-02-07 23:35:25 +0530

    svm and logistic regression added

--- app.py
+++ app.py
@@ -1,41 +1,68 @@
 import streamlit as st
 import tensorflow as tf
 from tensorflow.keras.models import load_model
-import pickle
-from tensorflow.keras.preprocessing.sequence import pad_sequences
+import joblib
 import numpy as np
+
+
+
+model_options = {
+    "Logistic Regression": "logistic_regression",
+    "LSTM": "lstm",
+    "Random Forest": "random_forest",
+    "SVM": "svm",
+    "XGBoost": "xgboost"
+}
+
 
 # Load the trained model
 
 
 @st.cache_resource
-def load_trained_model():
-    return load_model("models/neural_network/multi_label_model.h5")
+def load_trained_model(model_name):
+    if model_name == "logistic_regression":
+        domain_model = joblib.load("models/LogisticRegression/domain_model.pkl")
+        sub_domain_model = joblib.load("models/LogisticRegression/sub_domain_model.pkl")
+        return domain_model, sub_domain_model
+    # elif model_name == "lstm":
+    #     domain_model = load_model("models/LSTM/simple_lstm_domain.h5")
+    #     sub_domain_model = load_model("models/LSTM/simple_lstm_sub_domain.h5")
+    #     return domain_model, sub_domain_model
+    # elif model_name == "random_forest":
+    #     domain_model = joblib.load("models/RDF/optimized_random_forest_domain_1.pkl")
+    #     sub_domain_model = joblib.load("models/RDF/optimized_random_forest_sub_domain_1.pkl")
+    #     return domain_model, sub_domain_model
+    elif model_name == "svm":
+        domain_model = joblib.load("models/SVM/domain_model.pkl")
+        sub_domain_model = joblib.load("models/SVM/sub_domain_model.pkl")
+        return domain_model, sub_domain_model
+    # elif model_name == "xgboost":
+    #     domain_model = joblib.load("models/XGBoost/xgboost_domain.pkl")
+    #     sub_domain_model = joblib.load("models/XGBoost/xgboost_sub_domain.pkl")
+    #     return domain_model, sub_domain_model
+    else:
+        return None, None
 
-# Load Tokenizer
 
 
-@st.cache_data
-def load_tokenizer():
-    with open("models/neural_network/tokenizer.pkl", "rb") as f:
-        return pickle.load(f)
 
 # Load Label Encoders
 
 
 @st.cache_data
 def load_label_encoders():
-    with open("models/neural_network/domain_encoder.pkl", "rb") as f:
-        domain_encoder = pickle.load(f)
-    with open("models/neural_network/sub_domain_encoder.pkl", "rb") as f:
-        sub_domain_encoder = pickle.load(f)
-    return domain_encoder, sub_domain_encoder
+    return joblib.load("models/domain_label_encoder.pkl"), joblib.load("models/sub_domain_label_encoder.pkl")
+
+
+# Load Vectorizer
+
+
+@st.cache_data
+def load_vectorizer():
+    return joblib.load("models/tfidf_vectorizer.pkl")
 
 
 # Load model, tokenizer, and encoders
-model = load_trained_model()
-tokenizer = load_tokenizer()
-domain_encoder, sub_domain_encoder = load_label_encoders()
 
 # Streamlit UI
 st.title("🔍 Multi-Label Text Classification")
@@ -44,24 +71,36 @@ st.subheader("Classify text into Domain & Sub-Domain")
 # User input
 description = st.text_area("Enter a description:", "")
 
+
+# Dropdown menu for model selection
+selected_model_name = st.selectbox(
+    "Select a Model", list(model_options.keys()))
+selected_model = model_options[selected_model_name]
+
+
+model_domain, model_sub_domain = load_trained_model(selected_model)
+
+vectorizer = load_vectorizer()
+domain_encoder, sub_domain_encoder = load_label_encoders()
+
 # Predict function
 
 
 def predict_description(description):
     if description.strip():
         # Convert text to numerical sequence
-        seq = tokenizer.texts_to_sequences([description])
-        # Ensure it matches the model input
-        padded_seq = pad_sequences(seq, padding='post', maxlen=100)
-
+        vectorized_text = vectorizer.transform([description])
         # Get predictions
-        domain_pred, sub_domain_pred = model.predict(padded_seq)
+        domain_pred, sub_domain_pred = model_domain.predict(vectorized_text), model_sub_domain.predict(vectorized_text)
 
+        
         # Decode Predictions
         domain_label = domain_encoder.inverse_transform(
-            [np.argmax(domain_pred)])
+            domain_pred.astype(int))
         sub_domain_label = sub_domain_encoder.inverse_transform(
-            [np.argmax(sub_domain_pred)])
+            sub_domain_pred.astype(int))
+        
+        print(domain_label, sub_domain_label)
 
         return domain_label[0], sub_domain_label[0]
     return None, None
@@ -74,6 +113,10 @@ if st.button("Classify"):
 
     if domain_result and sub_domain_result:
         st.success(f"**Predicted Domain:** {domain_result}")
+        st.info(f"**Predicted Sub-Domain:** {sub_domain_result}")
+    elif domain_result:
+        st.success(f"**Predicted Domain:** {domain_result}")
+    elif sub_domain_result:
         st.info(f"**Predicted Sub-Domain:** {sub_domain_result}")
     else:
         st.warning("Please enter a valid description!")
---
+++ scripts/lr_svm_models.py
@@ -0,0 +1,153 @@
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+import pandas as pd
+import numpy as np
+import re
+import string
+import joblib
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import classification_report
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+import nltk
+import warnings
+
+from sklearn.svm import SVC
+warnings.filterwarnings("ignore")
+
+# Download required NLTK data
+nltk.download('stopwords')
+nltk.download('punkt')
+nltk.download('wordnet')
+
+# Load dataset
+# df = pd.read_csv("../Dataset/processed_data/final_dataset.csv")
+df = pd.read_csv("final_dataset.csv")
+print("Dataset loaded successfully.)")
+
+# Text Preprocessing
+
+
+def preprocess_text(text):
+    text = text.lower()
+    text = re.sub(f"[{string.punctuation}]", "", text)  # Remove punctuation
+    text = re.sub(r"\d+", "", text)  # Remove numbers
+    words = word_tokenize(text)
+    stop_words = set(stopwords.words('english'))
+    lemmatizer = WordNetLemmatizer()
+    words = [lemmatizer.lemmatize(word)
+             for word in words if word not in stop_words]
+    return " ".join(words)
+
+
+# Apply preprocessing
+df['clean_description'] = df['description'].astype(str).apply(preprocess_text)
+print("Text preprocessing completed.")
+
+# Label Encoding for Target Variables
+domain_encoder = LabelEncoder()
+sub_domain_encoder = LabelEncoder()
+df['domain_encoded'] = domain_encoder.fit_transform(df['domain'])
+df['sub_domain_encoded'] = sub_domain_encoder.fit_transform(df['sub_domain'])
+print("Label encoding completed.")
+
+
+# saving label encoder
+joblib.dump(domain_encoder, "../models/domain_label_encoder.pkl")
+joblib.dump(sub_domain_encoder, "../models/sub_domain_label_encoder.pkl")
+print("Label encoder saved successfully.)")
+
+# TF-IDF Vectorization
+tfidf = TfidfVectorizer(max_features=5000, ngram_range=(1, 2))
+X = tfidf.fit_transform(df['clean_description'])
+print("TF-IDF vectorization completed.")
+
+# Save vectorizer
+joblib.dump(tfidf, "../models/tfidf_vectorizer.pkl")
+print("Vectorizer saved successfully.)")
+
+y_domain = df['domain_encoded']
+y_sub_domain = df['sub_domain_encoded']
+
+
+
+print("Data split for domain prediction...")
+# DOMAIN
+# Split data for domain prediction
+X_train, X_test, y_train_domain, y_test_domain = train_test_split(
+    X, y_domain, test_size=0.2, random_state=42)
+print("Data split for domain prediction completed.")
+
+print("Logistic Regression Domain Model Training...")
+# LOGISTIC REGRESSION
+# Train Logistic Regression for domain prediction
+domain_model_lr = LogisticRegression(max_iter=500, solver='lbfgs')
+domain_model_lr.fit(X_train, y_train_domain)
+joblib.dump(domain_model_lr, "../models/LogisticRegression/domain_model.pkl")
+
+
+print("SVM Domain Model Training...)")
+# Train SVM for domain prediction
+domain_model_svm = SVC(kernel='linear', probability=True)
+domain_model_svm.fit(X_train, y_train_domain)
+joblib.dump(domain_model_svm, "../models/SVM/domain_model.pkl")
+
+
+print("Evaluation")
+# Evaluate
+y_pred_domain_lr = domain_model_lr.predict(X_test)
+print("Domain Prediction Accuracy For Logistic Regression:",
+      accuracy_score(y_test_domain, y_pred_domain_lr))
+
+# Evaluate
+y_pred_domain = domain_model_svm.predict(X_test)
+print("Domain Prediction Accuracy (SVM):",
+      accuracy_score(y_test_domain, y_pred_domain))
+
+
+# ---------------------------------------------------------------------------------------
+
+print("Data split for sub domain prediction...")
+# SUB DOMAIN
+# Split data for sub-domain prediction
+X_train, X_test, y_train_sub, y_test_sub = train_test_split(
+    X, y_sub_domain, test_size=0.2, random_state=42)
+print("Data split for sub domain prediction completed.")
+
+
+print("Logistic Regression Sub Domain Model Training...)")
+# LOGISTIC REGRESSION
+# Train Logistic Regression for sub-domain prediction
+sub_domain_model_lr = LogisticRegression(max_iter=500, solver='lbfgs')
+sub_domain_model_lr.fit(X_train, y_train_sub)
+joblib.dump(sub_domain_model_lr, "../models/LogisticRegression/sub_domain_model.pkl")
+
+
+print("SVM Sub Domain Model Training...")
+# Train SVM for sub-domain prediction
+sub_domain_model_svm = SVC(kernel='linear', probability=True)
+sub_domain_model_svm.fit(X_train, y_train_sub)
+joblib.dump(sub_domain_model_svm, "../models/SVM/sub_domain_model.pkl")
+
+
+print("Evaluation")
+# Evaluate
+y_pred_sub = sub_domain_model_lr.predict(X_test)
+print("Sub-Domain Prediction Accuracy For Logistic Regression:",
+      accuracy_score(y_test_sub, y_pred_sub))
+
+# Evaluate
+y_pred_sub = sub_domain_model_svm.predict(X_test)
+print("Sub-Domain Prediction Accuracy (SVM):",
+      accuracy_score(y_test_sub, y_pred_sub))
+
+
+
+
+
+print(" Model saved successfully!")
